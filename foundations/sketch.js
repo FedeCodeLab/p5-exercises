dec0cf635aac6de542a9dc43ab5f4cf171b347f7
FedeCodeLab <federicoguzman.css@gmail.com>
2025-11-16 19:57:35 -0300

    Foundations

---
+++ foundations/sketch.js
@@ -0,0 +1,19 @@
+/// <reference path="../p5.d.ts" />
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255);
+  fill(0);
+  noStroke();
+  if (mouseX < width / 3) {
+    rectMode(CENTER);
+    square(width / 2, height / 2, 400);
+  } else if (mouseX < (2 * width) / 3) {
+    circle(width / 2, height / 2, 400);
+  } else {
+    triangle(250, 50, 450, 450, 50, 450);
+  }
+}
